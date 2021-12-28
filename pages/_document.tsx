386423138e7366a43350fffbe0f3ff5aba8360d5
Alex Kibler <RKibler@ppg.com>
2021-12-28 15:55:04 -0500

    feat: added twitter meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,10 +8,16 @@ class MyDocument extends Document {
       <Html>
         <Head>
             <meta name="keyword" content="portfolio, developer, software engineer, javascript, typescript, react, nextjs, angular, dotnet" />
+
+            {/* Open Graph Meta Tags */}
             <meta property="og:title" content="Alex Kibler | Full Stack Web Developer"/>
             <meta property="og:image" content="https://alexkibler.rocks/images/me.png"/>
             <meta property="og:url" content="https://alexkibler.rocks" />
             <meta property="og:type" content="website" />
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content="Alex Kibler | Full Stack Web Developer" />
+            <meta name="twitter:image" content="https://alexkibler.rocks"/>
         </Head>
         <body>
           <Main />
